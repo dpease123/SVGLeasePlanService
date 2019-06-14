c8787c746cbcac80997d28a414b452eb231281c5
dpease123 <peaseman@gmail.com>
2019-06-14 01:35:58 -0400

    Configure Quartz job to run

---
+++ SVGLeasePlanService/Jobs/GenerateSVGJob.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Quartz;
+
+namespace SVGLeasePlanService.Jobs
+{
+    public class GenerateSVGJob : IJob
+    {
+        private static readonly log4net.ILog log = log4net.LogManager.GetLogger(System.Reflection.MethodBase.GetCurrentMethod().DeclaringType);
+        public async Task Execute(IJobExecutionContext context)
+        {
+
+            log.Info("Hello from GenerateSVGJob job!");
+           
+        }
+    }
+}
--- SVGLeasePlanService/Service.cs
+++ SVGLeasePlanService/Service.cs
@@ -6,6 +6,7 @@ using System.Threading.Tasks;
 using Quartz;
 using Quartz.Impl;
 using System.Collections.Specialized;
+using SVGLeasePlanService.Jobs;
 
 namespace SVGLeasePlanService
 {
@@ -42,8 +43,25 @@ namespace SVGLeasePlanService
                 // and start it off
                 await scheduler.Start();
 
+                // define the job and tie it to our HelloJob class
+                IJobDetail job = JobBuilder.Create<GenerateSVGJob>()
+                    .WithIdentity("SVGJob", "group1")
+                    .Build();
+
+                // Trigger the job to run now, and then repeat every 10 seconds
+                ITrigger trigger = TriggerBuilder.Create()
+                    .WithIdentity("trigger1", "group1")
+                    .StartNow()
+                    .WithSimpleSchedule(x => x
+                        .WithIntervalInSeconds(10)
+                        .RepeatForever())
+                    .Build();
+
+                // Tell quartz to schedule the job using our trigger
+                await scheduler.ScheduleJob(job, trigger);
+
                 // some sleep to show what's happening
-                await Task.Delay(TimeSpan.FromSeconds(60));
+                await Task.Delay(TimeSpan.FromSeconds(7));
 
                 // and last shut down the scheduler when you are ready to close your program
                 await scheduler.Shutdown();
