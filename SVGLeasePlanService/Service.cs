f3a25ad49248b8648497a563847bc2d9c122eabe
dpease123 <peaseman@gmail.com>
2019-06-14 00:46:29 -0400

    configure log4net logging

--- SVGLeasePlanService/Service.cs
+++ SVGLeasePlanService/Service.cs
@@ -8,13 +8,17 @@ namespace SVGLeasePlanService
 {
     public class Service
     {
+        private static readonly log4net.ILog log = log4net.LogManager.GetLogger(System.Reflection.MethodBase.GetCurrentMethod().DeclaringType);
         public void Start()
         {
-            // write code here that runs when the Windows Service starts up.  
+            // write code here that runs when the Windows Service starts up. 
+            log.Info("SVGLeasePlanService started.");
+            
         }
         public void Stop()
         {
             // write code here that runs when the Windows Service stops.  
+            log.Info("SVGLeasePlanService stopped.");
         }
     }
 }
