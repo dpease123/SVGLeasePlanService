54e8b84d99daa9ba020a96d0f144cfddce37b34e
dpease123 <41402420+dpease123@users.noreply.github.com>
2019-06-17 13:13:35 -0400

    fixed bugs - SVGs now work in Chrome

--- SVGLeasePlanService/SVGBuilder.cs
+++ SVGLeasePlanService/SVGBuilder.cs
@@ -15,17 +15,17 @@ namespace SVGLeasePlanService
         private static readonly log4net.ILog log = log4net.LogManager.GetLogger(System.Reflection.MethodBase.GetCurrentMethod().DeclaringType);
         private readonly LDBRepository _repo = new LDBRepository();
         StringBuilder sb = new StringBuilder();
+        private static string BldgId;
         public async Task Build()
         {
 
-            //Test for mike
             var PNGInputFolder = ConfigurationManager.AppSettings["PNGInputFolder"];
             var SVGOutputFolder = ConfigurationManager.AppSettings["SVGOutputFolder"];
+
             var pngFiles = Directory.GetFiles(@PNGInputFolder, "*.png");
             foreach (var png in pngFiles)
             {
                 GetCntrAbbrandFloorFromPNGName(png, out string PNGName, out dynamic x);
-
 
                 var FloorSpaces = await _repo.GetPolygonsByCenterandFloor(x.CtrAbbr, x.Floor);
 
@@ -36,21 +36,27 @@ namespace SVGLeasePlanService
                 }
 
                 log.Info("Building SVG floor plan for: " + x.CtrAbbr + "-" + "FloorNo: " + x.Floor + " | PNG File: " + png);
-                var stream = await File.OpenText(png).ReadToEndAsync();
-                sb.AppendLine($@"<svg version = ""1.1"" id=""Map"" class=""gen-by-synoptic-designer"" xmlns=""http://www.w3.org/2000/svg"" xmlns:xlink=""http://www.w3.org/1999/xlink"" viewBox=""0 0 4200 3000"" xml:space=""preserve""><image width = ""4200"" height=""3000"" xlink:href=""data:image/png;base64,{stream}"" />");
 
+                BldgId = FloorSpaces[0].Polygons[0].BldgId;
+
+                var PngBase64String = ConvertPNGtoBase64String(png);
+                sb.AppendLine($@"<svg version = ""1.1"" id=""Map"" class=""gen-by-synoptic-designer"" xmlns=""http://www.w3.org/2000/svg"" xmlns:xlink=""http://www.w3.org/1999/xlink"" viewBox=""0 0 4200 3000"" xml:space=""preserve""><image width = ""4200"" height=""3000"" xlink:href=""data:image/png;base64,{PngBase64String}"" />");
+
+                
                 foreach (var s in FloorSpaces)
                 {
-                    sb.AppendLine($@"<polygon id=""_x31_224 - {s.SuitId}"" title=""{s.SuitId} points="" ");
+                    
+                    sb.AppendLine($@"<polygon id=""{x.CtrAbbr}-{s.SuitId}"" title=""{s.SuitId}"" points="" ");
                     foreach (var p in s.Polygons)
                     {
-                        sb.AppendLine($"{p.X}, {p.Y}");
+                        sb.AppendLine($"{p.X.ToString("N0").Replace(",","")}, {p.Y.ToString("N0").Replace(",","")}");
                     }
                     sb.Append("\"");
                     sb.Append("/>");
                 }
                 sb.AppendLine("</svg>");
                 File.WriteAllText(@SVGOutputFolder + PNGName + ".svg", sb.ToString());
+                sb.Clear();
 
             }
 
@@ -61,6 +67,12 @@ namespace SVGLeasePlanService
             var f = new FileInfo(png);
             PNGName = f.Name.Replace(".png", "");
             x = Utilities.GetCenterFloorFromFileName(PNGName);
+        }
+
+        private static string ConvertPNGtoBase64String(string pngPath)
+        {
+            byte[] imageArray = System.IO.File.ReadAllBytes(@pngPath);
+            return Convert.ToBase64String(imageArray);
         }
     }
 }
