01ede35d5bc593456d9e11bb4d6b9b8e70007480
dpease123 <41402420+dpease123@users.noreply.github.com>
2019-06-24 14:04:53 -0400

    Refactor - slim down SVGWorker

--- SVGLeasePlanService/Jobs/Workers/SVGWorker.cs
+++ SVGLeasePlanService/Jobs/Workers/SVGWorker.cs
@@ -15,21 +15,20 @@ namespace SVGLeasePlanService
         private static readonly log4net.ILog log = log4net.LogManager.GetLogger(System.Reflection.MethodBase.GetCurrentMethod().DeclaringType);
         private readonly LDBRepository _repo = new LDBRepository();
         readonly StringBuilder sb = new StringBuilder();
-        private static string BldgId;
         public async Task Work()
         {
             var PNGInputFolder = ConfigurationManager.AppSettings["PNGInputFolder"];
             var SVGOutputFolder = ConfigurationManager.AppSettings["SVGOutputFolder"];
 
-            var pngFiles = Directory.GetFiles(@PNGInputFolder, "*.png");
+            string[] pngFiles = GetPNGPlansToProcess(PNGInputFolder);
 
             foreach (var png in pngFiles)
             {
                 GetCntrAbbrandFloorFromPNGName(png, out string PNGName, out dynamic x);
 
-                var Floorpolygons = await _repo.GetPolygonsByCenterandFloor(x.CtrAbbr, x.Floor);
+                var FloorTenantPolygons = await _repo.GetPolygonsByCenterandFloor(x.CtrAbbr, x.Floor);
 
-                if (Floorpolygons.Count == 0)
+                if (FloorTenantPolygons.Count == 0)
                 {
                     log.Info("***Skipping No polygon data found: " + x.CtrAbbr + "-" + "FloorNo: " + x.Floor + " | PNG File: " + png);
                     continue;
@@ -37,34 +36,54 @@ namespace SVGLeasePlanService
 
                 log.Info("Building SVG floor plan for: " + x.CtrAbbr + "-" + "FloorNo: " + x.Floor + " | PNG File: " + png);
 
-                GetBuildngId(Floorpolygons);
+                AddSVGFileHeader(ConvertPNGtoBase64String(png));
 
-                var PngBase64String = ConvertPNGtoBase64String(png);
-                sb.AppendLine($@"<svg version = ""1.1"" id=""Map"" class=""gen-by-synoptic-designer"" xmlns=""http://www.w3.org/2000/svg"" xmlns:xlink=""http://www.w3.org/1999/xlink"" viewBox=""0 0 4200 3000"" xml:space=""preserve""><image width = ""4200"" height=""3000"" xlink:href=""data:image/png;base64,{PngBase64String}"" />");
+                var BldgId = GetBuildngId(FloorTenantPolygons);
 
+                AddFloorTenannatPolygonsToSVGFile(x, FloorTenantPolygons, BldgId);
 
-                foreach (var s in Floorpolygons)
-                {
-                    sb.AppendLine($@"<polygon id=""{x.CtrAbbr}-{BldgId}"" title=""{s.SuitId}"" points="" ");
-                    //sb.AppendLine($@"<polygon id=""{x.CtrAbbr}-{s.SuitId}"" title=""{s.SuitId}"" points="" ");
-                    foreach (var p in s.Polygons)
-                    {
-                        sb.AppendLine($"{p.X.ToString("N0").Replace(",", "")}, {p.Y.ToString("N0").Replace(",", "")}");
-                    }
-                    sb.Append("\"");
-                    sb.Append("/>");
-                }
-                sb.AppendLine("</svg>");
-                File.WriteAllText(@SVGOutputFolder + PNGName + ".svg", sb.ToString());
+                WriteSVGFileToDisk(SVGOutputFolder, PNGName);
+
                 sb.Clear();
 
             }
 
         }
 
-        private static void GetBuildngId(dynamic FloorSpaces)
+        private void WriteSVGFileToDisk(string SVGOutputFolder, string PNGName)
         {
-            BldgId = FloorSpaces[0].Polygons[0].BldgId;
+            File.WriteAllText(@SVGOutputFolder + PNGName + ".svg", sb.ToString());
+        }
+
+        private void AddFloorTenannatPolygonsToSVGFile(dynamic x, dynamic Floorpolygons, string BldgId)
+        {
+            foreach (var s in Floorpolygons)
+            {
+                sb.AppendLine($@"<polygon id=""{x.CtrAbbr}-{BldgId}"" title=""{s.SuitId}"" points="" ");
+                //sb.AppendLine($@"<polygon id=""{x.CtrAbbr}-{s.SuitId}"" title=""{s.SuitId}"" points="" ");
+                foreach (var p in s.Polygons)
+                {
+                    sb.AppendLine($"{p.X.ToString("N0").Replace(",", "")}, {p.Y.ToString("N0").Replace(",", "")}");
+                }
+                sb.Append("\"");
+                sb.Append("/>");
+            }
+            sb.AppendLine("</svg>");
+        }
+
+        private void AddSVGFileHeader(string PngBase64String)
+        {
+            sb.AppendLine($@"<svg version = ""1.1"" id=""Map"" class=""gen-by-synoptic-designer"" xmlns=""http://www.w3.org/2000/svg"" xmlns:xlink=""http://www.w3.org/1999/xlink"" viewBox=""0 0 4200 3000"" xml:space=""preserve""><image width = ""4200"" height=""3000"" xlink:href=""data:image/png;base64,{PngBase64String}"" />");
+        }
+
+        private static string[] GetPNGPlansToProcess(string PNGInputFolder)
+        {
+            return Directory.GetFiles(@PNGInputFolder, "*.png");
+        }
+
+        private static string GetBuildngId(dynamic FloorSpaces)
+        {
+            return FloorSpaces[0].Polygons[0].BldgId;
         }
 
         private static void GetCntrAbbrandFloorFromPNGName(string png, out string PNGName, out dynamic x)
